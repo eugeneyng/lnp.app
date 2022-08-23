1cdbbe6c815597a5e3e0bb43713cba9833f7eeb0
eugene <ngyeugene@gmail.com>
2022-08-23 00:21:25 -0400

    moving back to bulma, basic gui setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
-
 import * as Pages from "./pages";
 
 function Volleyboard() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,104 @@
-import 'bootstrap';
-import React, { Component } from 'react';
+import "bulma/css/bulma.css";
+import * as React from "react";
+import defaults from "../assets/defaults.json";
 
-function Home() {
+export default function Home() {
+
   return (
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">
-          Card Title
-        </h5>
-        <p className="card-text">
-          Good at passing
-        </p>
+    <div className="hero is-fullheight">
+      <div className="hero-head">
+
+      </div>
+      <div className="hero-body">
+        <div className="container">
+
+          {/* This section is for form controls */}
+          <div className="section field is-grouped is-grouped-centered is-grouped-multiline is-mobile">
+
+            {/* <label className="label"># Players</label> */}
+            <div className="control">
+              <input className="input" type="number" placeholder="Number of Players"></input>
+            </div>
+
+            {/* <label className="label"># Subs</label> */}
+            <div className="control">
+              <input className="input" type="number" placeholder="Number of Subs"></input>
+            </div>
+          
+          </div>
+
+          {/* This section is for the opposing team */}
+          <div className="section">
+            <div className="columns is-mobile is-vcentered">
+              <RivalTeam />
+            </div>
+          </div>
+
+          {/* This section is for the net */}
+          <div className="section">
+            <hr />
+          </div>
+
+          {/* This section is for the friendly team */}
+          <div className="section">
+            <div className="columns is-mobile is-vcentered">
+              <FriendlyTeam />
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      <div className="hero-foot">
+
       </div>
     </div>
   );
-}
 
-export default Home;
+  function RivalTeam() {
+
+    function CreateRivalRotation() {
+      const [rivals, setRivals] = React.useState(defaults.players);
+
+      return rivals.map(createPlayerCard);
+    }
+
+    function createPlayerCard(player) {
+      return (
+        <div className="column">
+          <div className="card" key={player.id}>
+            {player.name}
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <CreateRivalRotation />
+    )
+
+  }
+
+  function FriendlyTeam() {
+
+    function CreateFriendRotation() {
+
+      const [friends, setFriends] = React.useState(defaults.players);
+
+      return friends.map(createPlayerCard);
+
+    }
+    function createPlayerCard(player) {
+      return (
+        <div className="column">
+          <div className="card" key={player.id}>
+            {player.name}
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <CreateFriendRotation />
+    )
+  }
+}
