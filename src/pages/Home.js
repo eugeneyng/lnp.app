e91c1d2a6ebb3a9a3a8172e3fa6450419c92e7bc
eugene <ngyeugene@gmail.com>
2022-08-28 21:03:16 -0400

    GUI updates, ability to change names, layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import defaults from "../assets/defaults.json";
 
 export default function Home() {
 
+  const [rivals, setRivals] = React.useState(defaults.players);
+  const [friends, setFriends] = React.useState(defaults.players);
+
   return (
     <div className="hero is-fullheight">
       <div className="hero-head">
@@ -12,38 +15,27 @@ export default function Home() {
       <div className="hero-body">
         <div className="container">
 
-          {/* This section is for form controls */}
-          <div className="section field is-grouped is-grouped-centered is-grouped-multiline is-mobile">
-
-            {/* <label className="label"># Players</label> */}
-            <div className="control">
-              <input className="input" type="number" placeholder="Number of Players"></input>
-            </div>
-
-            {/* <label className="label"># Subs</label> */}
-            <div className="control">
-              <input className="input" type="number" placeholder="Number of Subs"></input>
-            </div>
-          
-          </div>
+          {/* TODO: Use + and x buttons to add or delete players, instead of this form */}
 
           {/* This section is for the opposing team */}
-          <div className="section">
-            <div className="columns is-mobile is-vcentered">
-              <RivalTeam />
-            </div>
+          <div className="section py-0">
+            <RivalTeam />
           </div>
 
           {/* This section is for the net */}
-          <div className="section">
+          <div className="section py-0">
+            <hr />
+            <button>Rotate</button>
             <hr />
           </div>
 
           {/* This section is for the friendly team */}
-          <div className="section">
-            <div className="columns is-mobile is-vcentered">
-              <FriendlyTeam />
-            </div>
+          <div className="section py-0">
+            <FriendlyTeam />
+          </div>
+
+          <div className="section has-text-centered">
+            <button>Save</button>
           </div>
           
         </div>
@@ -54,54 +46,115 @@ export default function Home() {
     </div>
   );
 
+  function createPlayerCard(player) {
+    return (
+      <div className="card my-5 has-background-grey-lighter has-text-centered"
+           draggable="true"
+           key={player.name}
+           onDragOver={(event) => allowDrop(event)}
+           onDragStart={(event) => startDrag(event)}
+           onDrop={(event) => swap(event)}>
+        <div className="card-content">
+          <div className="title">
+            {player.name.charAt(0)}
+          </div>
+          <p contentEditable="true"
+             suppressContentEditableWarning="true"
+             onKeyDown={(event) => allowKeyDown(event, player)}>
+            {player.name}
+          </p>
+        </div>
+      </div>
+    )
+  }
+
+  function allowDrop(event) {
+    event.preventDefault(); // Default behavior is not to allow drop, so we will allow this
+  }
+  
+  function allowKeyDown(event, player) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      player.name = event.target.innerHTML.replace(/\br/g,'')
+      setFriends(friends);
+      setRivals(rivals);
+      event.target.blur();
+    }
+  }
+  
+  function startDrag(event) {
+    console.log("Dragging ...")
+  }
+
+  function swap(event) {
+    console.log("Swapping ...")
+    event.preventDefault();
+  }
+
   function RivalTeam() {
 
-    function CreateRivalRotation() {
-      const [rivals, setRivals] = React.useState(defaults.players);
-
-      let backrow = [rivals[0].name, rivals[1].name, rivals[2].name]
-      let frontrow = [rivals[3].name, rivals[4].name, rivals[5].name]
-
-      return rivals.map(createPlayerCard);
-    }
-
-    function createPlayerCard(player) {
-      return (
-        <div className="column">
-          <div className="card" key={player.id}>
-            {player.name} : {player.sub.toString()}
-          </div>
-        </div>
-      )
-    }
+    let rightsubs = [rivals[0]]
+    let rightside = [rivals[1], rivals[2]];
+    let middleide = [rivals[6], rivals[3]];
+    let leftside =  [rivals[5], rivals[4]];
+    let leftsubs = [rivals[7]]
 
     return (
-      <CreateRivalRotation />
+      <div className="columns is-mobile is-vcentered">
+        <div className="column is-one-fifth">
+          {rightsubs.map(createPlayerCard)}
+        </div>
+        <div className="box has-background-grey-light column">
+          <div className="columns is-mobile is-vcentered">
+            <div className="column">
+              {rightside.map(createPlayerCard)}
+            </div>
+            <div className="column">
+              {middleide.map(createPlayerCard)}
+            </div>
+            <div className="column">
+              {leftside.map(createPlayerCard)}
+            </div>
+          </div>
+        </div>
+        <div className="column is-one-fifth">
+          {leftsubs.map(createPlayerCard)}
+        </div>
+      </div>
     )
 
   }
 
   function FriendlyTeam() {
 
-    function CreateFriendRotation() {
-
-      const [friends, setFriends] = React.useState(defaults.players);
-
-      return friends.map(createPlayerCard);
-
-    }
-    function createPlayerCard(player) {
-      return (
-        <div className="column">
-          <div className="card" key={player.id}>
-            {player.name}
-          </div>
-        </div>
-      )
-    }
+    let rightsubs = [friends[7]]
+    let rightside = [friends[4], friends[5]];
+    let middleide = [friends[3], friends[6]];
+    let leftside =  [friends[2], friends[1]];
+    let leftsubs = [friends[0]]
 
     return (
-      <CreateFriendRotation />
+      <div className="columns is-mobile is-vcentered">
+        <div className="column is-one-fifth">
+          {rightsubs.map(createPlayerCard)}
+        </div>
+        <div className="box has-background-grey-light column">
+          <div className="columns is-mobile is-vcentered">
+            <div className="column">
+              {rightside.map(createPlayerCard)}
+            </div>
+            <div className="column">
+              {middleide.map(createPlayerCard)}
+            </div>
+            <div className="column">
+              {leftside.map(createPlayerCard)}
+            </div>
+          </div>
+        </div>
+        <div className="column is-one-fifth">
+          {leftsubs.map(createPlayerCard)}
+        </div>
+      </div>
     )
   }
 }
