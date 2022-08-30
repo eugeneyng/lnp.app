3134ae2c57b6bf3fec882ed57e7e6da206ac5c78
eugene <ngyeugene@gmail.com>
2022-08-30 01:26:04 -0400

    readme template

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import * as React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import * as Pages from "./pages";
 
-function Volleyboard() {
+function LNP() {
   return (
     <Pages.Home />
   );
 }
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
-root.render(<Volleyboard />);
+root.render(<LNP />);
